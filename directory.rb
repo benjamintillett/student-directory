d5ebdd02685dbb3d10f407418471cb15dc85ba2c
benjamintillett <benjamin.tillett@gmail.com>
2014-08-06 12:41:37 +0100

    add save function

--- directory.rb
+++ directory.rb
@@ -120,6 +120,7 @@ end
 def print_menu
 	puts "1. Input the students"
 	puts "2.Show the Students"
+	puts "3. Save the list to students.csv"
 	puts "9. Exit"
 end
 def show_students
@@ -134,6 +135,8 @@ def process(selection)
 			@students = input_students
 		when "2"
 			show_students
+		when "3"
+			save_students
 		when "9"
 			exit
 		else
@@ -141,14 +144,25 @@ def process(selection)
 	end
 end 
 
-
-
 def interactive_menu 
 	loop do 
 		print_menu
 		process(gets.chomp) 
 	end
 end
+
+def save_students
+	# open file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort],student[:hobby]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
+end
+
 
 
 
