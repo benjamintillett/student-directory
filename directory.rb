1dc953e28dfc2382419d4e47e965fc0cc3973375
benjamintillett <benjamin.tillett@gmail.com>
2014-08-08 21:53:08 +0100

    Compleate all exercises on student directory and finish refactoring

--- directory.rb
+++ directory.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
-
+require 'csv'
 @students = []
+@student = { name: "N/A", hobby: "N/A", cohort: "N/A"}
 
 def pluralize(n,singular,plural=nil)
 	if n == 1 
@@ -14,9 +15,8 @@ end
 
 def get_info(info)
 	puts "Please enter the next students #{info}"
-	temp_info = STDIN.gets.gsub(/\n/,"")
-	info = temp_info.empty? ? "N/A" : temp_info
-	return info
+	info = STDIN.gets.chomp
+	info.empty? ? "N/A" : info
 end
 
 def get_student_info	
@@ -24,7 +24,7 @@ def get_student_info
 	return if name == "quit"		
 	hobby = get_info("hobby")
 	return if hobby == "quit"
-	cohort = get_info("cohort").to_sym
+	cohort = get_info("cohort")
 	return if cohort == "quit"
 	[name, hobby, cohort]
 end
@@ -33,28 +33,25 @@ def add_student_info(name, cohort, hobby)
 	@students << { :name => name, :cohort => cohort.to_sym, :hobby => hobby }
 end
 
-
 def print_student(student,index)
 	puts "#{index+1}.#{student[:name]}".center(50) + "hobby: #{student[:hobby]}".center(50) + "(#{student[:cohort]} cohort)".center(50)
 end
 
 
 def input_students
-	puts "Please enter the names, hobbies and cohort of the students"
-	puts "To finish, fill in a field with quit and hit return"
+	puts "Please enter the names, hobbies and cohort of the students\nTo finish, fill in a field with quit and hit return"
 	while true do
 		name, hobby, cohort = get_student_info
 		break if [name, hobby, cohort].any? {|value| value == nil}
 		add_student_info(name, cohort, hobby)
 		puts "Now we have #{@students.length} students"
 	end
-	#return the array of students
-	@students
 end
 
 def print_header
 	puts "The #{pluralize(@students.length,'student')} of my cohort at Makers Academy"
-	puts "----------------"
+	puts "-----------------------------------"
+
 end
 
 def print_students_list
@@ -72,17 +69,16 @@ def get_cohort_list
 	@students.map { |student| student[:cohort] }.uniq
 end
 
-def print_by_cohort
-	cohort_list = get_cohort_list
-	cohort_list.each do |cohort| 
-		puts "Students in the #{cohort} cohort:  \n"
-		@students.each_with_index do |student,index| 
-			if student[:cohort] == cohort
-				print_student(student,index)
-			end
-		end
-	end
-end	
+def sort_students(info)
+	@students.sort_by! { |student| student[info.to_sym] }
+end
+
+def print_students_by_info(info)
+	puts "Showing all students sorted by #{info}"
+	puts "-----------------------------------"
+	sort_students(info)
+	print_students_list
+end
 
 def print_menu
 	puts "1. Input the students"
@@ -91,24 +87,21 @@ def print_menu
 	puts "4. Load Students from students.csv"
 	puts "9. Exit"
 end
+
 def show_students
 	print_header
-	print_students_list
+	puts "What do you want to sort by: name, cohort, hobby"
+	print_students_by_info(gets.chomp.to_sym)
 	print_footer
 end
 
 def process(selection)
 	case selection		
-		when "1"
-			@students = input_students
-		when "2"
-			show_students
-		when "3"
-			save_students
-		when "4"
-			load_students
-		when "9"
-			exit
+		when "1" then @students = input_students
+		when "2" then show_students
+		when "3" then save_students
+		when "4" then load_students
+		when "9" then exit
 		else
 			puts "I don't know what you meant, try again"
 	end
@@ -121,24 +114,23 @@ def interactive_menu
 	end
 end
 
+def get_filename
+	puts "please enter the file name"
+	gets.chomp 
+end
+
 def save_students
-	# open file for writing
-	File.open("students.csv", "w") do |file|
-		# iterate over the array of students
+	CSV.open(get_filename,"w") do |csv|
 		@students.each do |student|
-			student_data = [student[:name], student[:cohort],student[:hobby]]
-			csv_line = student_data.join(",")
-			file.puts csv_line
+			csv << student.values
 		end
 	end
 end
 
-def load_students(filename = "students.csv")
-	File.open(filename, "r"). do |file|
-		file.readlines.each do |line|
-			name, cohort, hobby = line.chomp.split(",")
-			add_student_info(name, cohort, hobby)
-		end
+def load_students
+	CSV.foreach(get_filename) do |line|
+		name, cohort, hobby = line
+		add_student_info(name, cohort, hobby)
 	end
 end
 
@@ -153,6 +145,10 @@ def try_load_students
 	end
 end 
 
+
 try_load_students
 interactive_menu
+
+
+
 
